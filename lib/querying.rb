57eefbd503515c47b5d8bc8916977e9b175998cb
Dani Schuhman <dani8439@aol.com>
2018-07-31 18:22:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT "
+  "SELECT books.titles, books.years"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  
+
 end
